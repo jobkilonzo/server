b105ec82852e364f07a2ddd83ea7167b3ec6cd85
jobkilonzo <jobkilonzo95@gmail.com>
2023-12-23 20:34:13 +0300

    Added openai

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express, { application } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import dalleRoutes from './routes/dalle.routes.js'
+
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({limig: "50mb"}))
+
+app.use('/api/v1/dalle', dalleRoutes)
 app.get('/', (req, res) => {
     res.status(200).json({message: "Hello from Job"})
 })
